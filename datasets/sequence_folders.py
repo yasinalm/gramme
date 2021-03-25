13ca134e1c7ae38acb41a276e49bcede3398c160
yasinalm <yasin_alm_58@hotmail.com>
2021-03-25 20:26:44 +0000

    matches radar and monocular frames

--- datasets/sequence_folders.py
+++ datasets/sequence_folders.py
@@ -1,9 +1,11 @@
+from typing import List
 import torch.utils.data as data
 import numpy as np
 # from skimage import io
 from PIL import Image
 import cv2
 from pathlib import Path
+from typing import List
 import random
 import radar
 
@@ -20,42 +22,44 @@ class SequenceFolder(data.Dataset):
 
     def __init__(
             self, root, skip_frames, sequence_length, train, dataset,
-            ro_params=None, mono_params=None,
-            seed=None, transform=None, sequence=None):
+            ro_params=None, load_mono=False,
+            seed=None, transform=None, mono_transform=None, sequence=None):
         np.random.seed(seed)
         random.seed(seed)
         self.root = Path(root)
         self.train = train
+        self.load_mono = load_mono
 
-        if ro_params is not None:
-            self.modality = 'radar'
-            self.isCartesian = ro_params['radar_format'] == 'cartesian'
+        # if ro_params is not None:
+        # self.modality = 'radar'
+        self.isCartesian = ro_params['radar_format'] == 'cartesian'
+        radar_folder = 'radar_cart' if self.isCartesian else 'radar'
+        if sequence is not None:
+            self.scenes = [self.root/sequence/radar_folder]
+        else:
+            scene_list_path = self.root/'train.txt' if train else self.root/'val.txt'
+            self.scenes = [self.root/folder.strip()/radar_folder for folder in open(scene_list_path)
+                           if not folder.strip().startswith("#")]
+            self.cart_resolution = ro_params['cart_resolution']
+            self.cart_pixels = ro_params['cart_pixels']
+            self.rangeResolutionsInMeter = ro_params['rangeResolutionsInMeter']
+            self.angleResolutionInRad = ro_params['angleResolutionInRad']
+            self.interpolate_crossover = True
+        if self.load_mono:
+            mono_folder = 'zed_left' if dataset == 'radiate' else 'stereo/centre'
+            # self.modality = 'mono'
             if sequence is not None:
-                radar_folder = 'radar_cart' if self.isCartesian else 'radar'
-                self.scenes = [self.root/sequence/radar_folder]
+                self.mono_scenes = [self.root/sequence/mono_folder]
             else:
                 scene_list_path = self.root/'train.txt' if train else self.root/'val.txt'
-                self.scenes = [self.root/folder.strip() for folder in open(scene_list_path)
-                               if not folder.strip().startswith("#")]
-                self.cart_resolution = ro_params['cart_resolution']
-                self.cart_pixels = ro_params['cart_pixels']
-                self.rangeResolutionsInMeter = ro_params['rangeResolutionsInMeter']
-                self.angleResolutionInRad = ro_params['angleResolutionInRad']
-                self.interpolate_crossover = True
-        elif mono_params is not None:
-            self.modality = 'mono'
-            if sequence is not None:
-                mono_folder = 'zed_left' if dataset == 'radiate' else 'stereo/centre'
-                self.scenes = [self.root/sequence/mono_folder]
-            else:
-                scene_list_path = self.root/'train_vo.txt' if train else self.root/'val_vo.txt'
-                self.scenes = [self.root/folder.strip() for folder in open(scene_list_path)
-                               if not folder.strip().startswith("#")]
-        else:
-            raise ValueError(
-                'Supported modality types are: [radar, mono], noth are None')
+                self.mono_scenes = [self.root/folder.strip()/mono_folder for folder in open(scene_list_path)
+                                    if not folder.strip().startswith("#")]
+        # else:
+        #     raise ValueError(
+        #         'Supported modality types are: [radar, mono], noth are None')
 
         self.transform = transform
+        self.mono_transform = mono_transform
         self.dataset = dataset
         self.k = skip_frames
         self.crawl_folders(sequence_length)
@@ -64,22 +68,47 @@ class SequenceFolder(data.Dataset):
         # k skip frames
         sequence_set = []
         demi_length = (sequence_length-1)//2
-        shifts = list(range(-demi_length * self.k,
-                            demi_length * self.k + 1, self.k))
-        shifts.pop(demi_length)
-        for scene in self.scenes:
+        self.shifts = list(range(-demi_length * self.k,
+                                 demi_length * self.k + 1, self.k))
+        self.shifts.pop(demi_length)
+        for s, scene in enumerate(self.scenes):
             # intrinsics = np.genfromtxt(scene/'cam.txt').astype(np.float32).reshape((3, 3))
             # f_type = '*.csv' if self.dataset == 'hand' else '*.png'
             f_type = '*.png'
             imgs = sorted(list(scene.glob(f_type)))
-
             if len(imgs) < sequence_length:
                 continue
+
             for i in range(demi_length * self.k, len(imgs)-demi_length * self.k):
                 # sample = {'intrinsics': intrinsics, 'tgt': imgs[i], 'ref_imgs': []}
                 sample = {'tgt': imgs[i], 'ref_imgs': []}
-                for j in shifts:
+                for j in self.shifts:
                     sample['ref_imgs'].append(imgs[i+j])
+
+                if self.load_mono:
+                    if self.dataset == 'radiate':
+                        imgs_mono = sorted(
+                            list(self.mono_scenes[s].glob(f_type)))
+                        if len(imgs) < sequence_length:
+                            continue
+
+                        f_rt = self.root/scene/'Navtech_Polar.txt'
+                        f_mt = self.root/scene/'zed_left.txt'
+
+                        rts = [float(folder.strip().split(':')[-1].strip())
+                               for folder in open(f_rt)]
+                        mts = [float(folder.strip().split(':')[-1].strip())
+                               for folder in open(f_mt)]
+
+                        mono_matches = self.find_mono_samples(i, rts, mts)
+                        if mono_matches is not None:
+                            # TODO: radar frame leri arasinda birden fazla monocular sequence var. Onlari sample a ekle.
+                            sample['vo_tgt_img'] = imgs_mono[mono_matches[0]]
+                            sample['vo_ref_imgs'] = [
+                                imgs_mono[ref] for ref in mono_matches[1:]]
+                    else:
+                        raise NotImplementedError(
+                            'Currently, only the RADIATE dataset is supported for VO')
                 sequence_set.append(sample)
 
         # Shuffle training dataset
@@ -122,6 +151,28 @@ class SequenceFolder(data.Dataset):
 
         return cart_img
 
+    def find_mono_samples(self, t_idx: int, rts: List[List[float]], mts: List[List[float]]) -> List[List[int]]:
+        """Returns indexes of monocular frames [[tgt, src-1, src+1], [tgt, src-1, src+1],...]
+
+        Args:
+            t_idx (int): Index of the target radar frame
+            rts (List[float]): List of radar timestamps
+            mts (List[float]): List of monocular timestamps
+
+        Returns:
+            List[List[int]]: Indexes of the matched monocular frames
+        """
+
+        # TODO: sadece 1 monocular sequence donderiyoz. Target ve source radar arasinda birden fazla monocular sequence var.
+        idxs = [find_nearest_mono_idx(rts[t_idx], mts)]
+        for s in self.shifts:
+            idxs.append(find_nearest_mono_idx(rts[t_idx+s], mts))
+
+        if any([i < 0 for i in idxs]) or len(set(idxs)) < len(self.shifts)+1:
+            return None
+
+        return idxs
+
     def __getitem__(self, index):
         sample = self.samples[index]
         # Choose the loader function
@@ -145,3 +196,31 @@ class SequenceFolder(data.Dataset):
 
     def __len__(self):
         return len(self.samples)
+
+
+def find_nearest_mono_idx(t: int, mts: List[List[float]]) -> int:
+    """Finds the nearest monocular timestamp for the given radar timestamp
+
+    Args:
+        t (int): Timestamp of the target frame
+        mts (List[List[float]]): List of monocular timestamps
+
+    Returns:
+        int: Index of the matched monocular frame
+    """
+
+    del_t = 0.050  # the match must be within 50ms of t
+    # First check if t is outside monocular frames but still within thr close
+    # Check if t comes before monocular frames
+    if t < mts[0]:
+        return 0 if mts[0]-t < del_t else -1
+    # Check if t comes after monocular frames
+    if t > mts[-1]:
+        return len(mts) if t-mts[-1] < del_t else -1
+    # Otherwise search within monocular frames
+    for i in range(len(mts)-1):
+        if t > mts[i] and t < mts[i+1]:
+            idx = i if (t-mts[i]) < (mts[i+1]-t) else i+1
+            idx = idx if abs(mts[idx]-t) < del_t else -1
+            return idx
+    return -1
