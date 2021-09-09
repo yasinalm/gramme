e2ebe54c7534413899f48795d7bb702f2bbab33f
Yasin Almalioglu <yasin_alm_58@hotmail.com>
2021-09-10 01:53:27 +0300

    test ro with test file

--- test_ro.py
+++ test_ro.py
@@ -30,12 +30,12 @@ parser.add_argument('--results-dir', default='results', metavar='PATH',
                     help='directory where to save predicted trajectories and stats')
 parser.add_argument('--resnet-layers',  type=int, default=18,
                     choices=[18, 50], help='number of ResNet layers for depth estimation')
-parser.add_argument('--with-pretrain', type=int,  default=0,
-                    help='with or without imagenet pretrain for resnet')
 parser.add_argument('--dataset', type=str, choices=[
-                    'hand', 'robotcar', 'radiate'], default='hand', help='the dataset to train')
-parser.add_argument("--sequence", default='2019-01-10-14-36-48-radar-oxford-10k-partial',
+                    'hand', 'robotcar', 'radiate'], default='robotcar', help='the dataset to train')
+parser.add_argument("--sequence", default='',
                     type=str, help="sequence to test")
+parser.add_argument('--with-testfile', type=int, default=0,
+                    help='use the test.txt file containing test sequences')
 parser.add_argument('--cam-mode', type=str, choices=[
                     'mono', 'stereo'], default='stereo', help='the dataset to train')
 # parser.add_argument('--pretrained-disp', dest='pretrained_disp', default=None, metavar='PATH', help='path to pre-trained dispnet model')
@@ -64,8 +64,8 @@ def main():
     global device
     args = parser.parse_args()
 
-    results_dir = Path(args.results_dir)/args.sequence
-    results_dir.mkdir(parents=True)
+    # results_dir = Path(args.results_dir)/args.sequence
+    # results_dir.mkdir(parents=True)
 
     print("=> fetching scenes in '{}'".format(args.data))
     ds_transform = custom_transforms.Compose(
@@ -79,74 +79,92 @@ def main():
         'radar_format': args.radar_format
     }
 
-    val_set = SequenceFolder(
-        args.data,
-        transform=ds_transform,
-        seed=args.seed,
-        train=False,
-        sequence_length=args.sequence_length,
-        skip_frames=args.skip_frames,
-        dataset=args.dataset,
-        ro_params=ro_params,
-        sequence=args.sequence,
-        radar_format=args.radar_format
-    )
-
-    val_loader = torch.utils.data.DataLoader(
-        val_set, batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True)
-
-    nframes = len(val_set)
-    print('{} samples found in {} valid scenes'.format(
-        len(val_set), len(val_set.scenes)))
-
     # create model
     print("=> creating model")
     weights_pose = torch.load(args.pretrained_pose)
     pose_net = models.PoseResNet(
-        args.dataset, args.resnet_layers, args.with_pretrain).to(device)
+        args.dataset, args.resnet_layers, False).to(device)
     pose_net.load_state_dict(weights_pose['state_dict'], strict=False)
+    pose_net = torch.nn.DataParallel(pose_net)
     pose_net.eval()
 
-    all_poses = []
-    all_inv_poses = []
-
-    t_del = 0
-    for i, (tgt_img, ref_imgs) in tqdm(enumerate(val_loader)):
-        #(tgt_img, ref_imgs) = val_it.next()
-
-        tgt_img = tgt_img.to(device)
-        ref_imgs = [img.to(device) for img in ref_imgs]
-
-        torch.cuda.synchronize()
-        inf_t0 = time.time()
-        poses, poses_inv = compute_pose_with_inv(pose_net, tgt_img, ref_imgs)
-        torch.cuda.synchronize()
-        t_del += time.time() - inf_t0
-
-        all_poses.append(poses)
-        all_inv_poses.append(poses_inv)
-
-    # Total time for forward and backward poses
-    print(
-        'Average time for inference: pair of frames/{:.2f}sec'.format(1./(t_del/(nframes*2))))
-
-    if args.with_gt:
-        print("=> converting odometry predictions to trajectory")
-        gt_file = Path(args.data, args.sequence, 'gt', 'radar_odometry.csv')
-        ro_eval = RadarEvalOdom(gt_file, args.dataset)
-        ate_f, f_pred_xyz, f_pred = ro_eval.eval_ref_poses(all_poses, all_inv_poses,
-                                                           args.skip_frames)
-        # save_traj_plots_with_gt(results_dir, f_pred_xyz, ro_eval.gt)
-        print("=> evaluating the trajectory")
-        isPartialSequence = 'partial' in args.sequence
-        odom_eval = EvalOdom(isPartial=isPartialSequence)
-        odom_eval.eval(f_pred.cpu().numpy(),
-                       ro_eval.gt.cpu().numpy(), results_dir)
+    if args.with_testfile:
+        root = Path(args.data)
+        scene_list_path = root/'test.txt'
+        sequences = [folder.strip()
+                     for folder in open(scene_list_path) if not folder.strip().startswith("#")]
+    elif args.sequence:
+        sequences = [args.sequence]
     else:
-        b_pred_xyz, f_pred_xyz = getTraj(
-            all_poses, all_inv_poses, args.skip_frames)
-        utils.save_traj_plots(results_dir, f_pred_xyz, b_pred_xyz)
+        raise argparse.ArgumentError(
+            'Neither a test file nor a sequence name given!')
+
+    for sequence in sequences:
+        results_dir = Path(args.results_dir)/sequence
+        results_dir.mkdir(parents=True)
+
+        print("=> fetching scenes in '{}'".format(Path(args.data)/sequence))
+        val_set = SequenceFolder(
+            args.data,
+            transform=ds_transform,
+            seed=args.seed,
+            train=False,
+            sequence_length=args.sequence_length,
+            skip_frames=args.skip_frames,
+            dataset=args.dataset,
+            ro_params=ro_params,
+            sequence=sequence,
+            radar_format=args.radar_format
+        )
+
+        val_loader = torch.utils.data.DataLoader(
+            val_set, batch_size=args.batch_size, shuffle=False,
+            num_workers=args.workers, pin_memory=True)
+
+        nframes = len(val_set)
+        print('{} samples found in {} valid scenes'.format(
+            len(val_set), len(val_set.scenes)))
+
+        all_poses = []
+        all_inv_poses = []
+
+        t_del = 0
+        for i, (tgt_img, ref_imgs) in tqdm(enumerate(val_loader)):
+            #(tgt_img, ref_imgs) = val_it.next()
+
+            tgt_img = tgt_img.to(device)
+            ref_imgs = [img.to(device) for img in ref_imgs]
+
+            torch.cuda.synchronize()
+            inf_t0 = time.time()
+            poses, poses_inv = compute_pose_with_inv(
+                pose_net, tgt_img, ref_imgs)
+            torch.cuda.synchronize()
+            t_del += time.time() - inf_t0
+
+            all_poses.append(poses)
+            all_inv_poses.append(poses_inv)
+
+        # Total time for forward and backward poses
+        print(
+            'Average time for inference: pair of frames/{:.2f}sec'.format(1./(t_del/(nframes*2))))
+
+        if args.with_gt:
+            print("=> converting odometry predictions to trajectory")
+            gt_file = Path(args.data, sequence, 'gt', 'radar_odometry.csv')
+            ro_eval = RadarEvalOdom(gt_file, args.dataset)
+            ate_f, f_pred_xyz, f_pred = ro_eval.eval_ref_poses(all_poses, all_inv_poses,
+                                                               args.skip_frames)
+            # save_traj_plots_with_gt(results_dir, f_pred_xyz, ro_eval.gt)
+            print("=> evaluating the trajectory")
+            isPartialSequence = 'partial' in args.sequence
+            odom_eval = EvalOdom(isPartial=isPartialSequence)
+            odom_eval.eval(f_pred.cpu().numpy(),
+                           ro_eval.gt.cpu().numpy(), results_dir)
+        else:
+            b_pred_xyz, f_pred_xyz = getTraj(
+                all_poses, all_inv_poses, args.skip_frames)
+            utils.save_traj_plots(results_dir, f_pred_xyz, b_pred_xyz)
 
 
 def compute_pose_with_inv(pose_net, tgt_img, ref_imgs):
